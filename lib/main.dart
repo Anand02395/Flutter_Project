9d7f3dd87b6be7686344614c4a2128f2410a3c6e
anand <anandgogawale2395@gmail.com>
2022-09-18 17:03:47 +0530

    day 1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,14 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var days = 300;
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Flutter Demo "),
-          ),
-        ),
-      ),
+      home: Homepage(), 
     );
   }
 }
